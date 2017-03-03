4434e40d037dcd1741ba6749c0832953313a337e
hyunckim <hyunkim1209@gmail.com>
2017-03-03 12:06:08 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  context: __dirname,
+  entry: "./widgets.jsx",
+  output: {
+      path: "./",
+      filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
